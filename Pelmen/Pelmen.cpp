b63328ef3497d30314d7d9f135e9614bddef3b9f
LVS-124 <Vovn149@mail.ru>
2025-04-28 19:11:32 +0300

    ТОЧНО ГОТОВО МУЖИКИ! ДОВАРИЛИСЬ!

--- Pelmen/Pelmen.cpp
+++ Pelmen/Pelmen.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
+    float Qp = 0, Qt = 0, Qf = 0, qp = 0, qf = 0, qt = 0, M = 0, t = 0, af = 0, np = 0, nf = 0, nt = 0;
+    cout << "Введите исходные данные:" << endl;
+    cout << "qp = ";
+    cin >> qp;
+    cout << "qf = ";
+    cin >> qf;
+    cout << "qt = ";
+    cin >> qt;
+    cout << "M = ";
+    cin >> M;
+    cout << "t = ";
+    cin >> t;
+    cout << "af = ";
+    cin >> af;
+    cout << "nf = ";
+    cin >> nf;
+    cout << "nt = ";
+    cin >> nt;
+    np = Qp / qp;
+    Qp = M / 2 * t;
     Qf = af * Qp;
     nf = Qf / qf;
     Qt = (1-af) * Qp;
