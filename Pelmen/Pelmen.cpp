43ceed35ede88bd7bbdaff66678d9b9ca560ca72
Anfitevien <Anfitevien@gmail.com>
2025-04-28 18:54:39 +0300

    Добавьте файлы проекта.

---
+++ Pelmen/Pelmen.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    Qf = af * Qp;
+    nf = Qf / qf;
+    Qt = (1-af) * Qp;
+    nf = Qt / qt;
+    cout << "Количество автоматов для отдела пельменей: " << np << endl;
+    cout << "Количество автоматов для отдела фарша: " << nf << endl;
+    cout << "Количество автоматов для отдела теста: " << nt << endl;
+}
+
